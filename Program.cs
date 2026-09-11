3ff7801339261453dadd4ada01e8114ac187fc5f
NandhiniJaganathan <nandhinij89@gmail.com>
2026-09-11 09:41:18 +0200

    Added a new method

--- Program.cs
+++ Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, GIT!");
+        Console.WriteLine(Firstmethod("Nan"));
+    }
+    static string Firstmethod(string name)
+    {
+        return $"Hello { name}";
     }
 }
